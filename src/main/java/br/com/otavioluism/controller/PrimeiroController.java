d134c4f983d69190257a6d6d676c7e1d23b481bd
otavioluism <otavioluism97@hotmail.com>
2024-10-09 21:44:28 -0300

    criando metodos relacionado ao path params e também ao query params

--- src/main/java/br/com/otavioluism/controller/PrimeiroController.java
+++ src/main/java/br/com/otavioluism/controller/PrimeiroController.java
@@ -1,16 +1,26 @@
 package br.com.otavioluism.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController // anotation para marcar a classe como uma api rest e sendo um controller
 @RequestMapping("/primeiroController") // anotation responsavel por mapear a classe como uma rota
 public class PrimeiroController {
 
-    @GetMapping("/primeiroMetodo") // anotation responsável para criar o tipo de http params para tal rota
-    public String primeiroMetodo() {
-        return "Parabéns, você criou seu primeiro método utilizando SpringBoot!";
+    @GetMapping("/metodoComPathParam/{id}") // anotation responsável para criar o tipo de http params para tal rota
+    public String metodoComPathParams(@PathVariable String id) {
+        return "O retorno do path param é = " + id;
+    }
+
+    @GetMapping("/metodoComQueryParam") // anotation responsável para criar o tipo de http params para tal rota
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O retorno do query param é = " + id;
+    }
+
+    @GetMapping("/metodoComQueryParam2") // anotation responsável para criar o tipo de http params para tal rota
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParam) {
+        return "O retorno do query param é = " + allParam;
     }
 
 
